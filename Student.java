8c81998f898ca4c3ddbb734b2df665ae9a053e1f
Noah-Hickey <Noah.Hickey@keyin.com>
2025-03-12 16:26:30 -0230

    Completed Teacher and Student class.

--- Student.java
+++ Student.java
@@ -1,3 +1,40 @@
-public class Student {
+public class Student extends Person{
     
+    // Attributes //
+    protected String myIdNum;
+    protected double myGPA;
+
+    // Constructors //
+    public Student(String name, int age, String gender, String idNum, double gpa) {
+        
+        super(name, age, gender);
+
+        myIdNum = idNum;
+        myGPA = gpa;
+    }
+
+    // Getters //
+    public String getIdNum() {
+        return myIdNum;
+    }
+
+    public double getGPA() {
+        return myGPA;
+    }
+
+    // Setters //
+    public void setIdNum(String idNum) {
+        myIdNum = idNum;
+    }
+
+    public void setGPA(double gpa) {
+        myGPA = gpa;
+    }
+
+    // toString Method //
+    @Override
+    public String toString() {
+    return super.toString() + ", ID: " + myIdNum + ", GPA: " + myGPA;
+    }
+
 }
--- Teacher.java
+++ Teacher.java
@@ -1,3 +1,37 @@
-public class Teacher {
+
+public class Teacher extends Person{
     
+    // Atrributes //
+    protected String subject;
+    protected double salary;
+
+    // Constructors //
+    public Teacher(String name, int age, String subject, double salary, String gender){
+        super(name,age,gender);
+        this.subject = subject;
+        this.salary = salary;
+    }
+
+    // Getters //
+    public String getSubject(){
+        return subject;
+    }
+
+    public double getSalary(){
+        return salary;
+    }
+
+    // Setters //
+    public void setSubject(String subject){
+        this.subject = subject;
+    }
+
+    public void setSalary(double salary){
+        this.salary = salary;
+    }
+
+    @Override 
+    public String toString(){
+        return super.toString() + ", Subject: " + subject + ", Salary: " + salary;
+    }
 }
